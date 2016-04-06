92bfed50320aa1ccc2d41475ace42dd520f2425a
Aaron Toys <aarontoys@gmail.com>
2016-04-06 13:38:14 -0600

    next commit

--- w14-d3-warm-up-http-api/controllers.js
+++ w14-d3-warm-up-http-api/controllers.js
@@ -1,13 +1,15 @@
 app.controller('pokemonController', function($scope, $http){
   $scope.myVar = 'Pokemon!';
-  $scope.pokemonImg = 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif';
-  $scope.moveImg = 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif';
-  
+  // $scope.pokemonImg = 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif';
+  // $scope.moveImg = 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif';
+  $scope.placeholderImg = 'http://www.kathanshah.com/mywork/wp-content/uploads/2015/09/jOLLXXR.gif'
+  $scope.showPokemon = false;
+  $scope.showMove = false;
   
 
   $scope.getPokemon = function () {
     $scope.rndPokemon = Math.floor(Math.random()*720)+1;
-    $scope.pokemonImg = 'http://www.kathanshah.com/mywork/wp-content/uploads/2015/09/jOLLXXR.gif'
+    $scope.show = true;
     $http({
       method: 'GET',
       url: 'http://pokeapi.co/api/v2/pokemon/'+$scope.rndPokemon
@@ -16,6 +18,7 @@ app.controller('pokemonController', function($scope, $http){
         console.log(results);
         $scope.pokemonName = results.data.name;
         $scope.pokemonImg = results.data.sprites.front_default;
+        $scope.show
       })
   }
 
