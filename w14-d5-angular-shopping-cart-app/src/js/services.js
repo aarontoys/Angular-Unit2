9276c2f7371a4cea1f0f4452d41b98507e71fce7
Aaron Toys <aarontoys@gmail.com>
2016-04-11 10:43:24 -0600

    remove console.logs

--- w14-d5-angular-shopping-cart-app/src/js/services.js
+++ w14-d5-angular-shopping-cart-app/src/js/services.js
@@ -11,18 +11,14 @@ app.service('shoppingService', function () {
     },
     saveItem: function (item, qty) {
       var filtered = cartItems.filter(function (el) {
-        console.log('before return')
         return item._id === el._id;
       });  
       filtered[0].qty = qty;
     },
     removeItem: function (item) {
-      console.log('cartItems:',cartItems);
       var removeIndex = cartItems.map(function (el) {
         return el._id; 
       }).indexOf(item._id);
-      console.log(removeIndex);
-      console.log(!!~removeIndex);
       ~removeIndex && cartItems.splice(removeIndex, 1);
     }
 
