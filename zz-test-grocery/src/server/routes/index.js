0f68f677b808b807c7de47a68fbe027b9a993da1
Aaron Toys <aarontoys@gmail.com>
2016-04-12 09:43:53 -0600

    loop still WIP

--- zz-test-grocery/src/server/routes/index.js
+++ zz-test-grocery/src/server/routes/index.js
@@ -12,10 +12,11 @@ var url = 'http://products.peapod.com/'+urlId+'.html'
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 
-  var cycleRequest = function (url){
+  // var cycleRequest = function (url){
 
-    request(url)
-      .then(function(html) {
+  //   request(url)
+  //     .then(function(html) {
+    var scrape = function (html, i) {
         var $ = cheerio.load(html);
         var img, name, size, upc, pageUrl;
         var data = $('.TEXT02').parent() ;
@@ -27,7 +28,7 @@ router.get('/', function(req, res, next) {
           name = newData.children().first().next().text(); 
           size = newData.children().first().next().next().text(); 
           upc = newData.children().first().next().next().next().next().text().trim(); 
-          pageUrl = 'tba';
+          pageUrl = i;
 
           queries.addGroceries(img,name,size,upc,pageUrl) 
             .then(function () {
@@ -37,12 +38,14 @@ router.get('/', function(req, res, next) {
               console.log('upc already in db');
             });
         }
-      })
-      .catch(function(err){
-        rejectArr.push(urlId);
+      }
+      // .catch(function(err){
+      var scrapeErr = function (err, i) {
+        rejectArr.push(i);
         console.log('rejectArr.length:', rejectArr.length);
-      });
-  }
+        console.log(rejectArr);
+      };
+  // }
 
   // cycleRequest(url);
 
@@ -50,21 +53,23 @@ router.get('/', function(req, res, next) {
   
   // uri: 'http://products.peapod.com/7732'+i+'.html',
 
-  for (var i = 1; i < 10; i++) {
+  for (var i = 10; i < 20; i++) {
     var options = {
-      uri: 'http://www.omdbapi.com/?i=tt156095'+i,
-      json: true
+      uri: 'http://products.peapod.com/7732'+i+'.html',
     };
     promiseArr.push(request(options)
-      .then(function(data){
-        console.log(data.Title + ' : ' + data.imdbID);
+      .then(function(html, i){
+        // console.log(data.Title + ' : ' + data.imdbID);
+        scrape(html);
       })
-      .catch(function(err){
-        console.log('There was an error: ',err)
+      .catch(function(err, i){
+        // console.log('There was an error: ',err)
+        scrapeErr(err);
       })
     );
   }
   console.log(promiseArr);
+  // console.log(rejectArr);
 });
 
 
